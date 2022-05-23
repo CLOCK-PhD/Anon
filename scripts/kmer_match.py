91321565a4262db40e2df374e58aba7137311944
Ositofeliz <remy.costa@live.fr>
2022-05-23 18:33:23 +0200

    poursuite des tests sur kmer_match.py

--- scripts/kmer_match.py
+++ scripts/kmer_match.py
@@ -20,6 +20,7 @@ import sys
 import argparse
 import os
 import heapq
+import linecache
 from Bio import SeqIO
 from typing import OrderedDict
 from pprint import pprint
@@ -30,8 +31,10 @@ def main():
     # Charger la séquence de référence en mémoire
     # A modifier plus tard pour l'input
     # Test sur le chromosome 22
+    seq_file = "../data/grch37p13/NC_000022.10_Homo_sapiens_chromosome_22.fasta"
+    kmer_file = "../data/kmer_snp/ch22/final_merge.tsv"
     seq = ""
-    with open("../data/grch37p13/NC_000022.10_Homo_sapiens_chromosome_22.fasta") as handle :
+    with open(seq_file) as handle :
         for record in SeqIO.parse(handle, "fasta"):
             seq = str(record.seq)
 
@@ -40,19 +43,19 @@ def main():
 
     # Utiliser la classe Automaton comme un trie
     # En gros on lui file les mots qu'on veut trouver ici
-    for idx, key in enumerate("ATCGGCCAGCAATCGCAAGGTT AAAAAAAAATTTTTTT AAAACCC AAAAAAAAAAAAAAAACCCCCCCCCC".split()):
-        automaton.add_word(key, (idx, key))
+    """for idx, key in enumerate("AAAAAAAAAAAAAAAAACCTC TTTTTTTTTTTTTTTTTTTGG AAAAAAAAAAAAAAAAAAAAC".split()):
+        automaton.add_word(key, (idx, key))"""
 
     # Vérifier si les mots sont dans l'automaton
-    print("he" in automaton)
-    print("HER" in automaton)
+    #print("he" in automaton)
+    #print("HER" in automaton)
 
     # Recherche dans l'automaton
     #print(automaton.get("he"))
     #print(automaton.get("she"))
 
     # Convertir le trie en un automaton Aho-Corasick pour permettre la recherche Aho-Corasick
-    automaton.make_automaton()
+    #automaton.make_automaton()
 
     #haystack = "AAAAAAAAAATTTTCCCAAAAATTGCCCCGTTTCAAACCCAACTAGGGACAGCAGTTAGCAT"
 
@@ -63,20 +66,43 @@ def main():
     where a trie key was found in the input string and the associated value for this key.
     Here we had stored as values a tuple with the original string and its trie insertion order
     """
+    """for end_index, (insert_order, original_value) in automaton.iter(seq):
+        start_index = end_index - len(original_value) + 1
+        print((start_index, end_index, (insert_order, original_value)))
+        assert seq[start_index:start_index + len(original_value)] == original_value
+"""
+
+    #print("le chat")
+
+
+    # Fichier kmer
+    
+    # Test sur l'ouverture du fichier k-mer
+    count = 0
+    with open(kmer_file, "r") as f :
+        for line in f:
+            if count < 10000000 :
+                kmer = line.split("\t")[0]
+                #print(f"Ligne {count} - kmer : {kmer}")
+                automaton.add_word(kmer, (count, kmer))
+                count += 1
+            else : break
+
+    automaton.make_automaton()
     for end_index, (insert_order, original_value) in automaton.iter(seq):
         start_index = end_index - len(original_value) + 1
         print((start_index, end_index, (insert_order, original_value)))
         assert seq[start_index:start_index + len(original_value)] == original_value
+    #automaton.remove_word(kmer)
+    #print(automaton.get_stats())
 
 
-    print("le chat")
+    #the_file = open(kmer_file, "r")
 
-    # Test sur l'ouverture du fichier k-mer
-    with open("../data/kmer_snp/ch22/final_merge.tsv", "r") as kmer_file :
-        print("kmer file opened")
-        """for line in kmer_file:
-            print(line.split("\t")[0])"""
-        # Première partie des tests pour voir si on peut faire tourner avec le fichier
+    
+
+    """for line in the_file:
+        print(line.split("\t")[0])"""
 
 if __name__ == "__main__" :
     main()
