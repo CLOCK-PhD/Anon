4521b8c0e5993afaaa2cf6a9763f13d59ddfd8d8
Ositofeliz <remy.costa@live.fr>
2022-05-28 17:46:24 +0200

    Correction du problème avec le break mal placé

--- scripts/snp_kmer_finder.py
+++ scripts/snp_kmer_finder.py
@@ -59,16 +59,10 @@ def main():
     prefix_size = args.prefix_size
     output_unknown_prefix = args.no_pref
 
-    # Variables
-    #ksize = 21
-    #prefix_size = 5
-    #index_dir = "../data/snp_chr_name_test"
     found_kmer_dict = {}
     not_in_index = []
 
-    # Charger la séquence
     print("Loading sequence")
-    #seq_file = "../data/grch37p13/NC_000024.9_Homo_sapiens_chromosome_Y.fasta"
     with open(seq_file) as handle :
         for record in SeqIO.parse(handle, "fasta"):
             seq = str(record.seq).upper()
@@ -87,15 +81,12 @@ def main():
         try :
             with open(f"{index_dir}/{prefix}", "r")as f:
                 for line in f:
-                    # Pas de répétition => break une fois le k-mer trouvé dans l'indexe
                     if suffix == line.split("\t")[0] :
                         try:
                             found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])].append(i)
-                            break
                         except KeyError:
                             found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])] = [i]
-                            break
-                    break
+                        break
         except FileNotFoundError:
             not_in_index.append(prefix)
 
@@ -105,7 +96,7 @@ def main():
     found_kmer_dict = OrderedDict(sorted(found_kmer_dict.items()))
 
     # Afficher le k-mer et le nombre de fois qu'il apparait dans la séquence
-    with open(f"{index_dir}/05_kmer_finder_results.tsv", "w") as f:
+    with open(f"{index_dir}/07_kmer_finder_results.tsv", "w") as f:
         for key, value in found_kmer_dict.items() :
             f.write(f"{key[0]}\t{key[1]}\t{len(value)}\n")
 
