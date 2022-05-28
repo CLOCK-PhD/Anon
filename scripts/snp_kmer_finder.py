db38e7a9c92806296596e6802f400231ca99418e
Ositofeliz <remy.costa@live.fr>
2022-05-28 11:18:32 +0200

    Gestion des erreurs de création de fichier, barre de progression, modif commentaires

--- scripts/snp_kmer_finder.py
+++ scripts/snp_kmer_finder.py
@@ -21,13 +21,7 @@ IDEE : Actualiser l'indexe :
     2. Réutiliser le programme avec la séquence d'une autre personne.
 """
 
-import ahocorasick
-import re
-import sys
-import argparse
-import os
-import heapq
-import linecache
+from tqdm import tqdm
 from Bio import SeqIO
 from typing import OrderedDict
 from pprint import pprint
@@ -39,17 +33,22 @@ def main():
     prefix_size = 5
     index_dir = "../data/snp_chr_name_test"
     found_kmer_dict = {}
+    not_in_index = []
 
     # Charger la séquence
+    print("Loading sequence")
     seq_file = "../data/grch37p13/NC_000024.9_Homo_sapiens_chromosome_Y.fasta"
     with open(seq_file) as handle :
         for record in SeqIO.parse(handle, "fasta"):
             seq = str(record.seq).upper()
     
+    print("Analysing sequence k-mers...")
     # Parcourir les kmers de la séquence
     n_kmers = len(seq) - ksize + 1
     count = 0
+    pbar = tqdm(total=100000)
     for i in range(n_kmers):
+        pbar.update(1)
         count += 1
         kmer = str(seq[i:i+ksize])
         prefix = kmer[:prefix_size]
@@ -58,18 +57,32 @@ def main():
             with open(f"{index_dir}/{prefix}", "r")as f:
                 for line in f: # On changera ça pour une recherche dichotomique après
                     if suffix == line.split("\t")[0] :
-                        try:
+                        # Key = k-mer, Value = [rs_id]
+                        """try:
                             found_kmer_dict[prefix+line.split("\t")[0]].append(line.split("\t")[1])
                         except KeyError:
-                            found_kmer_dict[prefix+line.split("\t")[0]] = [line.split("\t")[1]]
+                            found_kmer_dict[prefix+line.split("\t")[0]] = [line.split("\t")[1]]"""
+                        # Key = (k-mer, rs_id), Value = comptage
+                        """try:
+                            found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])] += 1
+                        except KeyError:
+                            found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])] = 1"""
+                        # Key = (k-mer, rs_id), Value = [positions]
+                        try:
+                            found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])].append(i)
+                        except KeyError:
+                            found_kmer_dict[(prefix+line.split("\t")[0], line.split("\t")[1])] = [i]
         except FileNotFoundError:
-            print(f"{prefix} is not a prefix file")
+            not_in_index.append(prefix)
         if count == 100000:
             break
 
-    #pprint(found_kmer_dict)
-    for key, value in found_kmer_dict.items() :
-        print(f"{key} : {len(value)}")
+    pbar.close()
+
+    pprint(not_in_index)
+    # Afficher le k-mer et le nombre de fois qu'il apparait dans la séquence
+    """for key, value in found_kmer_dict.items() :
+        print(f"{key} : {value}")"""
 
 
 if __name__ == "__main__":
