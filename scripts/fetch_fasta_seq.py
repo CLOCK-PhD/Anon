2ec2dbfe2f1ccb933bba336b18be25899e7e60c3
Ositofeliz <remy.costa@live.fr>
2022-12-11 13:12:27 +0100

    Simplification des noms de fichiers en sortie

--- scripts/fetch_fasta_seq.py
+++ scripts/fetch_fasta_seq.py
@@ -5,6 +5,21 @@
 from hashlib import new
 import argparse
 from Bio import SeqIO
+import re
+
+# Conversion du nom du chromosome
+def convertChromName(c:str)->str:
+    res = re.search("NC_00+([0-9]{1,2}).*$", c)
+    if res :
+        chrom = res.group(1)
+        if res.group(1) == "23":
+            return "X"
+        elif res.group(1) == "24":
+            return "Y"
+        else :
+            return res.group(1)
+    else :
+        return c
 
 # Gestion des arguments
 parser = argparse.ArgumentParser()
@@ -19,8 +34,8 @@ input_file = args.fasta_file
 with open(input_file) as handle :
     for record in SeqIO.parse(handle, "fasta"):
         if "NC" in record.description: # Pour récupérer les séquences qui commencent par NC
-            output_name = record.description.split(",")[0]
-            output_name = output_name.replace(" ", "_")
+            output_name = convertChromName(record.description)
+            #output_name = output_name.replace(" ", "_")
             print(output_name)
             with open(f"{output_name}.fasta", "w") as output_handle:
                 SeqIO.write(record, output_handle, "fasta")
