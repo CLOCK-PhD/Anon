d7ee617129f4140889469332c6cd69a7211fd5fd
Ositofeliz <remy.costa@live.fr>
2022-12-12 20:07:02 +0100

    Nouvelle version objet de kmer_snp_gen

---
+++ scripts/ksg.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""
+ksg.py : K-mer SNP Generator
+
+Version orientée objet de kmer_snp_gen_index.py
+
+On copie plein de trucs de l'ancien fichier et on voit comment ça se passe ici.
+"""
+
