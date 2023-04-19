96f42e4dea50ece9146843307414823e7e72be2b
Ositofeliz <remy.costa@live.fr>
2023-04-19 20:01:32 +0200

    Création du programme pour rechercher les k-mers depuis les reads et commentaires

---
+++ scripts/anon.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+anon.py
+
+Programme qui va chercher les reads dans l'index des k-mers porteurs de SNP.
+
+Retourne des informations quant aux k-mers contenus dans le read, relatifs aux
+risque de ré-identification.
+
+"""
